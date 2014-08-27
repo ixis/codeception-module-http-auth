51e7beb22ebc85a0f08290122293471875d69805
Paul Byrne <paul.byrne@ixis.co.uk>
2014-08-27 13:01:27 +0100

    Misconfigured tests should now pass. Needs refactoring into environment-based config and single suite.

--- tests/acceptance-misconfigured/HttpAuthenticationFailCest.php
+++ tests/acceptance-misconfigured/HttpAuthenticationFailCest.php
@@ -8,7 +8,7 @@ class HttpAuthenticationFailCest
     /**
      * The string we expect see on the test site after authenticating.
      */
-    const HTTPAUTH_EXPECTED_PAGE_CONTENT = 'Welcome';
+    const HTTPAUTH_EXPECTED_PAGE_CONTENT = 'This location IS protected by HTTP authentication.';
 
     /**
      * Test response after failing authentication is 401.
@@ -18,7 +18,7 @@ class HttpAuthenticationFailCest
      */
     public function testFailedAuthenticatedResponse(MisconfiguredWebGuy $I)
     {
-        $I->amOnPage('/');
+        $I->amOnPage('/protected/');
         $I->seeResponseCodeIs(401);
     }
 
@@ -30,7 +30,7 @@ class HttpAuthenticationFailCest
      */
     public function seeContentAfterAuthentication(MisconfiguredWebGuy $I)
     {
-        $I->amOnPage('/');
+        $I->amOnPage('/protected/');
         $I->dontSee(self::HTTPAUTH_EXPECTED_PAGE_CONTENT);
     }
 }
