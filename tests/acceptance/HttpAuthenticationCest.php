acedaf9551f6945ad72e0ef42abb7a09287d78c0
Paul Byrne <paul.byrne@ixis.co.uk>
2014-08-27 12:47:33 +0100

    Change to public domain/path for testing; test we can see appropriate response and content on a NON-protected page.

--- tests/acceptance/HttpAuthenticationCest.php
+++ tests/acceptance/HttpAuthenticationCest.php
@@ -6,9 +6,37 @@
 class HttpAuthenticationCest
 {
     /**
-     * The string we expect see on the test site after authenticating.
+     * The path to visit for content that is NOT protected by HTTP authentication.
      */
-    const HTTPAUTH_EXPECTED_PAGE_CONTENT = 'Welcome';
+    const HTTP_AUTH_NON_PROTECTED_PATH = '/';
+
+    /**
+     * The path to visit for content that IS protected by HTTP authentication.
+     */
+    const HTTP_AUTH_PROTECTED_PATH = '/protected/';
+
+    /**
+     * The string we expect see on the non-protected page.
+     */
+    const HTTP_AUTH_EXPECTED_NON_PROTECTED_PAGE_CONTENT = 'This location is NOT protected by HTTP authentication.';
+
+    /**
+     * The string we expect see on the protected page after authenticating.
+     */
+    const HTTP_AUTH_EXPECTED_PROTECTED_PAGE_CONTENT = 'This location IS protected by HTTP authentication.';
+
+    /**
+     * Test response on a page without HTTP authentication enabled.
+     *
+     * @param WebGuy $I
+     *   The Guy object being used to test.
+     */
+    public function testNonProtectedPageResponse(WebGuy $I)
+    {
+        $I->amOnPage(self::HTTP_AUTH_NON_PROTECTED_PATH);
+        $I->seeResponseCodeIs(200);
+        $I->see(self::HTTP_AUTH_EXPECTED_NON_PROTECTED_PAGE_CONTENT);
+    }
 
     /**
      * Test response after authentication is 200.
@@ -18,7 +46,7 @@ class HttpAuthenticationCest
      */
     public function testAuthenticatedResponse(WebGuy $I)
     {
-        $I->amOnPage('/');
+        $I->amOnPage(self::HTTP_AUTH_PROTECTED_PATH);
         $I->seeResponseCodeIs(200);
     }
 
@@ -30,7 +58,7 @@ class HttpAuthenticationCest
      */
     public function seeContentAfterAuthentication(WebGuy $I)
     {
-        $I->amOnPage('/');
-        $I->see(self::HTTPAUTH_EXPECTED_PAGE_CONTENT);
+        $I->amOnPage(self::HTTP_AUTH_PROTECTED_PATH);
+        $I->see(self::HTTP_AUTH_EXPECTED_PROTECTED_PAGE_CONTENT);
     }
 }
